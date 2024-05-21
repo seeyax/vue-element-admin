9f61709f5f5cf5f2042056f51b9e333a22377235
seeya <seeya@sztus.com>
2024-05-21 16:44:06 +0800

    first commit

---
+++ src/store/modules/permission.js
@@ -0,0 +1,79 @@
+import { asyncRoutes, constantRoutes } from '@/router'
+
+/**
+ * Use meta.role to determine if the current user has permission
+ * @param roles
+ * @param route
+ */
+
+/**
+ * Array.every() 与Array.some()的区别
+ * every： 检测数组中所有元素是否符合指定条件，符合返回true，否则false
+ * some：检测数组中所有元素是否满足制定条件，满足返回true，否则false
+ * @param roles
+ * @param route
+ * @returns {boolean|*}
+ */
+function hasPermission(roles, route) {
+  if (route.meta && route.meta.roles) {
+    return roles.some(role => route.meta.roles.includes(role))
+  } else {
+    return true
+  }
+}
+
+/**
+ * Filter asynchronous routing tables by recursion
+ * @param routes asyncRoutes
+ * @param roles
+ */
+export function filterAsyncRoutes(routes, roles) {
+  const res = []
+
+  routes.forEach(route => {
+    // 深拷贝
+    const tmp = { ...route }
+    if (hasPermission(roles, tmp)) {
+      if (tmp.children) {
+        tmp.children = filterAsyncRoutes(tmp.children, roles)
+      }
+      res.push(tmp)
+    }
+  })
+
+  return res
+}
+
+const state = {
+  routes: [],
+  addRoutes: []
+}
+
+const mutations = {
+  SET_ROUTES: (state, routes) => {
+    state.addRoutes = routes
+    state.routes = constantRoutes.concat(routes)
+  }
+}
+
+const actions = {
+  generateRoutes({ commit }, roles) {
+    return new Promise(resolve => {
+      let accessedRoutes
+      if (roles.includes('admin')) {
+        accessedRoutes = asyncRoutes || []
+      } else {
+        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
+      }
+      commit('SET_ROUTES', accessedRoutes)
+      resolve(accessedRoutes)
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
---
+++ src/views/guide/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app-container">
+    <aside>
+      The guide page is useful for some people who entered the project for the first time. You can briefly introduce the
+      features of the project. Demo is based on
+      <a href="https://github.com/kamranahmedse/driver.js" target="_blank">driver.js.</a>
+    </aside>
+    <!--aside标签-->
+    <el-button icon="el-icon-question" type="primary" @click.prevent.stop="guide">
+      Show Guide
+    </el-button>
+  </div>
+</template>
+
+<script>
+import Driver from 'driver.js' // import driver.js
+import 'driver.js/dist/driver.min.css' // import driver.js css
+import steps from './steps'
+
+export default {
+  name: 'Guide',
+  data() {
+    return {
+      driver: null
+    }
+  },
+  mounted() {
+    this.driver = new Driver()
+    // this.driver = new Driver({
+    //   prevBtnText: '上一步',
+    //   nextBtnText: '下一步',
+    //   doneBtnText: '完成',
+    //   closeBtnText: '关闭',
+    //   allowClose: Boolean,
+    //   animate: '动画',
+    //   opacity: 0.5, // 遮罩层
+    //   padding: 10,
+    //   keyboardControl: false, // 禁止键盘操作
+    //   onHighlightStarted: (e) => {
+    //     //样式
+    //   }
+    // })
+  },
+  methods: {
+    // 点击操作指引
+    guide() {
+      this.driver.defineSteps(steps)
+      this.driver.start()
+    }
+  }
+}
+</script>
